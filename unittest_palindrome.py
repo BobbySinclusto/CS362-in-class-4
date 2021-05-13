f08c64705559c771385fad89495485053ca22030
BobbySinclusto <allencbenjamin@gmail.com>
2021-05-13 16:50:06 -0700

    added unittest tests for palindrome

---
+++ unittest_palindrome.py
@@ -0,0 +1,19 @@
+import unittest
+import palindrome
+
+class TestCasePalindrome(unittest.TestCase):
+    # ADD
+    def test_palindrome_good(self):
+        self.assertEqual(palindrome.is_palindrome('testing'), False)
+        self.assertEqual(palindrome.is_palindrome('bob'), True)
+        self.assertEqual(palindrome.is_palindrome('racecar'), True)
+        self.assertEqual(palindrome.is_palindrome('pot top'), True)
+    
+    def test_palindrome_fail(self):
+        self.assertEqual(palindrome.is_palindrome('this is definitely not a palindrome'), True)
+    
+    def test_palindrome_exceptions(self):
+        self.assertRaises(TypeError, palindrome.is_palindrome, 234)
+
+if __name__ == '__main__':
+    unittest.main()
